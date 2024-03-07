4b11e7c38a71ea4671b658edfe135e66b69cf012
liujingx <liujingx21@mails.tsinghua.edu.cn>
2024-03-07 13:57:23 +0800

    feat: random ai

---
+++ ai/src/main.rs
@@ -0,0 +1,15 @@
+use rand::random;
+
+fn main() {
+    let stdin = std::io::stdin();
+    loop {
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let output: &str = [
+            r#"{"gesture": Rock}"#,
+            r#"{"gesture": Scissors}"#,
+            r#"{"gesture": Paper}"#,
+        ][random::<usize>() % 3];
+        println!("{}", output);
+    }
+}
